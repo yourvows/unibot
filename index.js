7a01073fa04bd9511b92462f7912518c0951971c
IBOBRO <ibrohimlogos@gmail.com>
2022-12-21 01:18:24 +0500

    commit final

--- index.js
+++ index.js
@@ -1,73 +1,117 @@
-// const TelegramApi = require('node-telegram-bot-api')
-//
-// const token = '5890317465:AAHJn7gXLRZDIgmnc7JiSmYqzDTCn-zO92Q'
-//
-// const bot = new TelegramApi(token, {polling: true})
-//
-// bot.onText(RegExp("message"), msg => {
-//     bot.on("polling_error", console.log);
-//     const text = msg.text;
-//     console.log(msg);
-//     if (text === "/start") {
-//         bot.sendMessage(chat.id, `hello user`)
-//     } else {
-//         bot.sendMessage(chat.id, `you textded me ${first_name}`)
-//     }
-//     const bye = "bye";
-//     if (msg.text.toString().toLowerCase().includes(bye)) {
-//         bot.sendMessage(chat.id, "Hope to see you around again , Bye");
-//     }
-// })
+const path = require("path");
+const { readdir } = require("fs/promises");
+const axios = require("axios");
+const driver = require("selenium-webdriver");
+const { Builder, By, Key, untill } = require("selenium-webdriver");
+const TelegramBot = require("node-telegram-bot-api");
+const token = "5879394130:AAGw9NgBEpycnISeNDfUP5VF_sYjf11UUYQ";
+const bot = new TelegramBot(token, { polling: true });
+// get request
+let defLink = " front-view-generic-brandless-moder-car ";
 
+let downloadFolder = process.env.USERPROFILE + "/Downloads";
 
-const TelegramBot = require('node-telegram-bot-api');
-const token = '5890317465:AAHJn7gXLRZDIgmnc7JiSmYqzDTCn-zO92Q'
-const bot = new TelegramBot(token, {polling: true});
+function getIdFromLink(link) {
+  let splitted = link.split(".htm");
+  let id = splitted[0].split("_")[splitted.length - 1];
+  return id;
+}
+
+function getAliasFromLink(link) {
+  let splitted = link.split(".htm");
+  let fileLink = splitted[0].split("_")[0];
+  let fileLinkSplit = fileLink.split("/");
+  let alias = fileLinkSplit[fileLinkSplit.length - 1];
+  return alias;
+}
+
+const findByName = async (dir, name) => {
+  const matchedFiles = [];
+  const files = await readdir(dir);
+
+  for (const file of files) {
+    // Method 1:
+    const filename = path.parse(file).name;
+    let splittedName = filename.split(" ")[0];
+    if (splittedName === name) {
+      matchedFiles.push(file);
+    }
+  }
+  return matchedFiles;
+};
+
+function linkHandler(value) {
+  return new Promise(async (resolve, reject) => {
+    let driver = await new Builder().forBrowser("chrome").build();
+    if (value) {
+      let link = getIdFromLink(value);
+      try {
+        await driver.get(
+          "https://id.freepikcompany.com/v2/log-in?client_id=freepik&lang=en&_ga=2.22590587.1573958854.1671131491-470343965.1671131491"
+        );
+        await driver
+          .findElement(
+            By.css(
+              ".main-button.button.button--outline.button--with-icon:nth-child(3)"
+            )
+          )
+          .click();
+        let email_field = await driver.findElement(By.name("email"));
+        let password_field = await driver.findElement(By.name("password"));
+        let checkbox_field = await driver.findElement(By.name("keep-signed"));
+        await checkbox_field.click();
+        await email_field.sendKeys("zafar.azimov@davrbank.uz");
+        await password_field.sendKeys("Ddrake140ckm!");
+        let submit_button = await driver.findElement(
+          By.css('.main-button.button[type="submit"]')
+        );
+        await submit_button.click();
+        setTimeout(() => {
+          driver
+            .get(`https://www.freepik.com/download-file/${link}`)
+            .then(() => {
+              let countInterval = 0;
+              let interval = setInterval(async () => {
+                let aliasFile = getAliasFromLink(value);
+                let files = await findByName(downloadFolder, aliasFile);
+                let fileName = "";
+
+                if (files.length > 0) {
+                  fileName = files[files.length - 1];
+                  resolve(downloadFolder + "/" + fileName);
+                  driver.quit();
+                } else {
+                  countInterval++;
+                  if (countInterval > 15) {
+                    clearInterval(interval);
+                    reject("File not found");
+                    driver.quit();
+                  }
+                }
+              }, 2000);
+              // resolve("File not found");
+            });
+        }, 10000);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  });
+}
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
-    console.log(msg);
-    const chatId = msg.chat.id;
-    const resp = match[1];
-    bot.sendMessage(chatId, resp);
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  bot.sendMessage(chatId, resp);
 });
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    let replyText = msg.text
-    console.log(msg);
-    console.log(replyText)
-    if (replyText != "/start") {
-        bot.sendMessage(chatId, `hello ${replyText}`)
-    }else{
-        bot.sendMessage(chatId, `idi naxuy`)
-    }
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  let replyText = msg.text;
+  if (replyText != "/start") {
+    const filepath = await linkHandler(replyText);
+    // bot.sendMessage(chatId, `hello ${filepath}`);
+    bot.sendDocument(chatId, filepath);
+  } else {
+    bot.sendMessage(chatId, `Добро пожаловать ${msg.from.username} `);
+  }
 });
-
-// const TelegramBot = require('node-telegram-bot-api');
-//
-// // replace the value below with the Telegram token you receive from @BotFather
-// const token = '5890317465:AAHJn7gXLRZDIgmnc7JiSmYqzDTCn-zO92Q';
-//
-// // Create a bot that uses 'polling' to fetch new updates
-// const bot = new TelegramBot(token, {polling: true});
-//
-// // Matches "/echo [whatever]"
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//     // 'msg' is the received Message from Telegram
-//     // 'match' is the result of executing the regexp above on the text content
-//     // of the message
-//
-//     const chatId = msg.chat.id;
-//     const resp = match[1]; // the captured "whatever"
-//
-//     // send back the matched "whatever" to the chat
-//     bot.sendMessage(chatId, resp);
-// });
-//
-// // Listen for any kind of message. There are different kinds of
-// // messages.
-// bot.on('message', (msg) => {
-//     const chatId = msg.chat.id;
-//
-//     // send a message to the chat acknowledging receipt of their message
-//     bot.sendMessage(chatId, 'Received your message');
-// });
